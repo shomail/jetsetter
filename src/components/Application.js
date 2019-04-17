5c342a7c6e662a6ba7ec94e17f3d5dae0d7c5563
Shomail <shomail1988@gmail.com>
2019-04-17 22:42:15 +0200

    undo redo container implemented

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,6 +5,7 @@ import UnpackedItemsContainer from '../containers/UnpackedItemsContainer';
 import MarkAllAsUnpackedContainer from '../containers/MarkAllAsUnpackedContainer';
 import UnpackedFilterContainer from '../containers/UnpackedFilterContainer';
 import PackedFilterContainer from '../containers/PackedFilterContainer';
+import UndoRedoContainer from '../containers/UndoRedoContainer';
 
 import './Application.css';
 
@@ -13,6 +14,7 @@ class Application extends Component {
     return (
       <div className="Application">
         <NewItemContainer />
+        <UndoRedoContainer />
         <UnpackedItemsContainer title="Unpacked Items" render={() => <UnpackedFilterContainer />} />
         <PackedItemsContainer title="Packed Items" render={() => <PackedFilterContainer />} />
         <MarkAllAsUnpackedContainer />
