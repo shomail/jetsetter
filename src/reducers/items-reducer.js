df6a31d83facbfcf2c93556501fd96e028597702
Shomail <shomail1988@gmail.com>
2019-04-17 22:55:44 +0200

    fixed state tree ref

--- src/reducers/items-reducer.js
+++ src/reducers/items-reducer.js
@@ -1,26 +1,45 @@
 import { ADD_NEW_ITEM, REMOVE_ITEM, TOGGLE_ITEM, MARK_ALL_AS_UNPACKED } from '../constants';
 
 export default function(state = {}, action) {
+  const { past, present, future } = state;
+
   if (action.type === ADD_NEW_ITEM) {
-    const item = action.item;
-    return [ ...state, item ];
+    const {id, packed, value} = action;
+    return {
+      past: [present, ...past],
+      present: [...present, { id, packed, value }],
+      future,
+
+    };
   }
 
   if (action.type === REMOVE_ITEM) {
-    return state.filter(item => item.id !== action.id);
+    return {
+      past: [present, ...past],
+      present: present.filter(item => item.id !== action.id),
+      future,
+    }
   }
 
   if (action.type === TOGGLE_ITEM) {
-    return state.map(item => {
-      if (item.id === action.id) return { ...item, packed: !item.packed  };
-      return item;
-    });
+    return {
+      past: [present, ...past],
+      present: present.map(item => {
+        if (item.id === action.id) return { ...item, packed: !item.packed  };
+        return item;
+      }) ,
+      future,
+    }
   }
 
   if (action.type === MARK_ALL_AS_UNPACKED) {
-    return state.map(item => {
-      return { ...item, packed: false  };
-    });
+    return {
+      past: [present, ...past],
+      present: present.map(item => {
+        return { ...item, packed: false  };
+      }),
+      future,
+    }
   }
 
   return state;
