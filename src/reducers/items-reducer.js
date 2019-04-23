372a68b83c797b7df176b07b33bf63e1cba78f6e
Shomail <shomail1988@gmail.com>
2019-04-23 22:50:14 +0200

    items reducer updated for thunk

--- src/reducers/items-reducer.js
+++ src/reducers/items-reducer.js
@@ -1,74 +1,30 @@
-import { ADD_NEW_ITEM, REMOVE_ITEM, TOGGLE_ITEM, MARK_ALL_AS_UNPACKED, UNDO_ITEM_ACTION, REDO_ITEM_ACTION, UPDATE_ALL_ITEMS } from '../constants';
+import { UPDATE_ALL_ITEMS, ADD_NEW_ITEM, REMOVE_ITEM, TOGGLE_ITEM, MARK_ALL_AS_UNPACKED } from '../constants';
 
 export default function(state = {}, action) {
-  const { past, present, future } = state;
-
   if (action.type === UPDATE_ALL_ITEMS) {
-    return {
-      past,
-      present: action.items,
-      future,
-    }
+    return action.items;
   }
-  
+
   if (action.type === ADD_NEW_ITEM) {
-    const item = action.item;
-    return {
-      past: [present, ...past],
-      present: [...present, item],
-      future,
-    };
+    const { id, packed, value } = action.item;
+    return [ ...state, { id, packed, value } ];
   }
 
   if (action.type === REMOVE_ITEM) {
-    return {
-      past: [present, ...past],
-      present: present.filter(item => item.id !== action.id),
-      future,
-    }
+    return state.filter(item => item.id !== action.id);
   }
 
   if (action.type === TOGGLE_ITEM) {
-    return {
-      past: [present, ...past],
-      present: present.map(item => {
-        if (item.id === action.id) return { ...item, packed: !item.packed  };
-        return item;
-      }) ,
-      future,
-    }
+    return state.map(item => {
+      if (item.id === action.item.id) return { ...action.item };
+      return item;
+    });
   }
 
   if (action.type === MARK_ALL_AS_UNPACKED) {
-    return {
-      past: [present, ...past],
-      present: present.map(item => {
-        return { ...item, packed: false  };
-      }),
-      future,
-    }
-  }
-
-  if (action.type === UNDO_ITEM_ACTION) {
-    if (!past.length) return state;
-    const newFuture = [ present, ...future ];
-    const [ newPresent, ...newPast ] = past;
-    return {
-      past: newPast,
-      present: newPresent,
-      future: newFuture
-    }
-  }
-
-  if (action.type === REDO_ITEM_ACTION) {
-    if (!future.length) return state;
-    const [newPresent, ...newFuture] = future;
-    const newPast = [ present, ...past ];
-    return {
-      past: newPast,
-      present: newPresent,
-      future: newFuture
-    }
+    return state.map(item => {
+      return { ...item, packed: false  };
+    });
   }
 
   return state;
